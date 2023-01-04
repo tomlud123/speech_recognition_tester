95b96bccc3dcab2b8d0ccb9b6c1af7f6dae8bdad
tomlud <tomlud111@gmail.com>
2023-01-04 21:41:51 +0100

    update dependencies, configure app for test files

---
+++ app/src/test/java/SpeechToJsonTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+public class SpeechToJsonTest {
+
+    @org.junit.Test
+    public void getJsonString() {
+        int a = 1;
+        a++;
+        assertEquals(a,2);
+    }
+}
